1ac8874657770592ed7970adb2a147cc21697ab1
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-30 09:11:40 +0530

    .

---
+++ Entry VScode.py
@@ -0,0 +1,3 @@
+#this is the first program of Qubit Quantum Computing
+
+print("This is Aash Gates learning Quantum Computing along with some python")
---
+++ The 3 Inst.py
@@ -0,0 +1,10 @@
+#instruction said at 23:23 Lecture 6, 11.22.20
+
+name = "Aash Gates"
+age = 23
+height = 5.5
+fav_food= "Briyani"
+
+print ("Name :",name, "\nage :",age, "\nheight :",height, "\nFavrouite Food:",fav_food )
+
+#end-of-the-program
