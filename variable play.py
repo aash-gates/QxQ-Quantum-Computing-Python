e2ac3874b0ba922b65e4c346d032c7f9f3b8530d
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-30 23:13:55 +0530

    Create variable play.py

---
+++ variable play.py
@@ -0,0 +1,15 @@
+#variable test Lecture 6 QXQ
+
+dessert = "cheesecake"
+
+#one way of printing the variable
+
+print("I love" +dessert)
+
+#another way of printing the variable
+import time
+time.sleep(1.25)
+
+print("I love",dessert)
+
+#end of the program 
